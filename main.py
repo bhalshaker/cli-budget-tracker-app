9e9906dbb0447d8e4c5bb23370ac46a10e045c5f
Ebrahim F Alshaker <ebrahim.alshakar@gmail.com>
2025-02-08 12:33:48 +0300

    Document main.py

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import os
 from dotenv import load_dotenv
 
 def initiliaze_env():
+    """
+    This method initializes the environment variables.
+    """
     load_dotenv()
     data.DataController.data_base_dir= os.getenv('BUDG_DATA_PATH') if os.getenv('BUDG_DATA_PATH') else os.getcwd()
 
 
 def main():
+    """
+    This is the main method of the program.
+    """
     initiliaze_env()
     data.DataController.create_all_none_created_csv()
     data.DataController.load_data_from_files()
