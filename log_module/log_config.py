9e259a49ff4911695c5ef37229d20d10f5dde459
Ebrahim Alshaker <alshaker@proton.me>
2025-02-15 07:12:43 +0300

    Create log directory if it does not exists

--- log_module/log_config.py
+++ log_module/log_config.py
@@ -5,6 +5,8 @@ import os
 
 def setup_logging(logging_base_dir=None):
     logging_base_dir=logging_base_dir if logging_base_dir else os.path.join(os.getcwd(),'log')
+    if not os.path.exists(logging_base_dir): 
+        os.makedirs(logging_base_dir) 
     """
     Setup application logging
     """
